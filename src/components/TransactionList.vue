15898cccf0f1bc669a197da1799adcf9577afc2a
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-22 15:57:26 +0100

    Update

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -14,13 +14,17 @@ const color = computed((price) => {
 <template>
 <div class="mb-4">
   <h3 class="font-semibold py-1 border-b-2 mb-2 text-base">History</h3>
-  <div class="flex bg-white pl-2 justify-between shadow-md my-2" v-for="(item, index) in items" :key="item.id">
-    <p class="m-2">{{ item.name }}</p>
-    <div class="flex">
-      <p class="m-2">${{item.price}}</p>
-      <div :class="`bg-${item.price < 0 ? 'red' : 'green'}-400 w-1 h-full`"></div>
+  <div  v-for="(item, index) in items" :key="item.id" class="flex items-center">
+    <font-awesome-icon :icon="['fas', 'xmark']" class="text-red-300 w-6"/>
+    <div class="flex bg-white pl-2 justify-between shadow-md my-2 w-11/12">
+      <p class="m-2">{{ item.name }}</p>
+      <div class="flex">
+        <p class="m-2">${{item.price}}</p>
+        <div :class="`bg-${item.price < 0 ? 'red' : 'green'}-400 h-full`"></div>
+      </div>
     </div>
   </div>
+
 </div>
 </template>
 

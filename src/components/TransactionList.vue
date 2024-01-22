30a0aba3cf994df02e1add85b2780c935fb3f755
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-22 14:59:52 +0100

    Update TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -3,11 +3,14 @@
 </script>
 
 <template>
-<div>
-  <h3>History</h3>
-  <div class="flex">
-    <p>Sneakers</p>
-    <p>$900</p>
+<div class="mb-4">
+  <h3 class="font-semibold py-1 border-b-2 mb-2">History</h3>
+  <div class="flex bg-white pl-2 justify-between shadow-md">
+    <p class="m-2">Sneakers</p>
+    <div class="flex">
+      <p class="m-2">$900</p>
+      <div class="bg-red-400 w-1 h-full"></div>
+    </div>
   </div>
 </div>
 </template>

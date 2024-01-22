c01b2fe302d1d327a775e2b072dbb3d9100cdd61
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-22 15:11:36 +0100

    Add balance props

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+const {balance} = defineProps(["balance"])
 </script>
 
 <template>
   <div class="mb-3">
     <h2 class="text-base">Your Balance</h2>
-    <h3 class="font-semibold text-2xl">$600</h3>
+    <h3 class="font-semibold text-2xl">${{balance}}</h3>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,17 @@ import Balance from "../components/Balance.vue";
 import TransactionList from "../components/TransactionList.vue";
 import IncomeExpense from "../components/IncomeExpense.vue";
 import AddTransaction from "../components/AddTransaction.vue";
+
+import {ref, onMounted} from "vue";
+
+const balance = ref(600)
 </script>
 
 <template>
   <div class="flex justify-center items-center pt-10 font-poppins">
     <div class="flex flex-col">
       <Header/>
-      <Balance/>
+      <Balance :balance="balance"/>
       <IncomeExpense/>
       <TransactionList/>
       <AddTransaction/>

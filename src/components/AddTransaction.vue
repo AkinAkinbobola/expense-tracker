8dc15dddca7f3a9ac61b70f0945249fcd6200caf
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-22 15:01:43 +0100

    Update

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="flex flex-col">
-    <h1>Add new Transactions</h1>
+    <h1 class="font-semibold text-base py-1 border-b-2 mb-2">Add new Transactions</h1>
     <h3>Text</h3>
     <input type="text" placeholder="Enter Text...">
     <h3>Amount <br>(negative - expense, positive - income)</h3>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -4,7 +4,7 @@
 
 <template>
 <div class="mb-4">
-  <h3 class="font-semibold py-1 border-b-2 mb-2">History</h3>
+  <h3 class="font-semibold py-1 border-b-2 mb-2 text-base">History</h3>
   <div class="flex bg-white pl-2 justify-between shadow-md">
     <p class="m-2">Sneakers</p>
     <div class="flex">

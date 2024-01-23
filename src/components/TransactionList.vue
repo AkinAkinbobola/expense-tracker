a4b031f0497664efd2b0f7658d08c0ad7f6a4947
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-23 08:21:42 +0100

    Update TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -15,7 +15,7 @@ const color = computed((price) => {
 <div class="mb-4">
   <h3 class="font-semibold py-1 border-b-2 mb-2 text-base">History</h3>
   <div  v-for="(item, index) in items" :key="item.id" class="flex items-center">
-    <font-awesome-icon :icon="['fas', 'xmark']" class="text-red-300 w-6"/>
+    <font-awesome-icon :icon="['fas', 'xmark']" class="text-red-300 w-6 px-2 py-1 bg-red-400 text-white h-full mr-3"/>
     <div class="flex bg-white pl-2 justify-between shadow-md my-2 w-11/12">
       <p class="m-2">{{ item.name }}</p>
       <div class="flex">
@@ -27,7 +27,3 @@ const color = computed((price) => {
 
 </div>
 </template>
-
-<style scoped>
-
-</style>

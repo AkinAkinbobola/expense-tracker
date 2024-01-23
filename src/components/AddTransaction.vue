4fb1e788c9195bd7245bbce62e7f16f51886a94a
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-23 09:32:57 +0100

    Update AddTransaction.vue

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -5,10 +5,13 @@ const {item, price} = defineProps(["item", "price"])
 <template>
   <div class="flex flex-col">
     <h1 class="font-semibold text-base py-1 border-b-2 mb-2">Add new Transactions</h1>
-    <h3 class="text-base mb-2">Text</h3>
-    <input type="text" placeholder="Enter Text..." class="py-2 px-3 mb-2">
-    <h3 class="text-base mb-2">Amount <br>(negative - expense, positive - income)</h3>
-    <input type="text" placeholder="Enter Amount..." class="py-2 px-3 mb-4">
-    <button type="submit" class="bg-purple-500 text-white py-2">Add Transaction</button>
+    <form class="flex flex-col">
+      <label class="text-base mb-2" for="name">Text</label>
+      <input type="text" placeholder="Enter Text..." class="py-2 px-3 mb-2" id="name">
+      <label class="text-base mb-2" for="amount">Amount <br>(negative - expense, positive - income)</label>
+      <input type="text" placeholder="Enter Amount..." class="py-2 px-3 mb-4" id="amount">
+      <button type="submit" class="bg-purple-500 text-white py-2">Add Transaction</button>
+    </form>
+
   </div>
 </template>

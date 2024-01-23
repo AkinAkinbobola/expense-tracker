1e55fe1ca2edf5454fa164026369c88646b33a6e
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-23 09:43:12 +0100

    Packages and v-model

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,6 +1,10 @@
 <script setup>
+import {ref} from "vue";
+
+const name = ref("");
+const amount = ref("");
 const onSubmit = () => {
-  console.log("Submit")
+
 }
 </script>
 
@@ -9,11 +13,10 @@ const onSubmit = () => {
     <h1 class="font-semibold text-base py-1 border-b-2 mb-2">Add new Transactions</h1>
     <form class="flex flex-col" @submit.prevent="onSubmit">
       <label class="text-base mb-2" for="name">Text</label>
-      <input type="text" placeholder="Enter Text..." class="py-2 px-3 mb-2" id="name">
+      <input type="text" placeholder="Enter Text..." class="py-2 px-3 mb-2" id="name" v-model="name">
       <label class="text-base mb-2" for="amount">Amount <br>(negative - expense, positive - income)</label>
-      <input type="text" placeholder="Enter Amount..." class="py-2 px-3 mb-4" id="amount">
+      <input type="text" placeholder="Enter Amount..." class="py-2 px-3 mb-4" id="amount" v-model="amount">
       <button type="submit" class="bg-purple-500 text-white py-2 mb-5">Add Transaction</button>
     </form>
-
   </div>
 </template>
